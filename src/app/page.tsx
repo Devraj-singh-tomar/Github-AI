64249c841e00f56856f291c950fa0d0d624c4f0f
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2025-02-06 18:35:15 +0530

    setup, feat: project setup(prisma, nextjs, neon) and complete auth.

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default async function Home() {
+  return (
+    <div className="text-green-600">
+      <Button>heeasd</Button>
+      hello
+    </div>
+  );
+}
