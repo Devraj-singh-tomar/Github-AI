80e4cfb72d8306354002a6662239b38ab383dff8
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2025-02-14 08:45:55 +0530

    setup, feat :firebase store and meetingCard ui & upload files functionality

---
+++ src/app/(protected)/dashboard/meeting-card.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { uploadFile } from "@/lib/firebase";
+import { MonitorIcon, UploadCloudIcon } from "lucide-react";
+import { useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+
+const MeetingCard = () => {
+  const [progress, setProgress] = useState(0);
+  const [isUploading, setIsUploading] = useState(false);
+
+  const { getInputProps, getRootProps } = useDropzone({
+    accept: {
+      "audio/*": [".mp3", ".wav", ".m4a"],
+    },
+    multiple: false,
+    maxFiles: 50_000_000,
+
+    onDrop: async (acceptedFiles) => {
+      setIsUploading(true);
+
+      console.log(acceptedFiles);
+
+      const file = acceptedFiles[0];
+
+      const downloadURL = await uploadFile(file as File, setProgress);
+
+      window.alert(downloadURL);
+
+      setIsUploading(false);
+    },
+  });
+
+  return (
+    <Card
+      className="col-span-2 flex flex-col items-center justify-center"
+      {...getRootProps()}
+    >
+      {isUploading ? (
+        <div className="flex flex-col items-center">
+          <CircularProgressbar
+            value={progress}
+            text={`${progress}%`}
+            className="mb-3 size-10"
+            styles={buildStyles({
+              pathColor: "#7C3AED",
+              textColor: "#7C3AED",
+              textSize: "0px",
+            })}
+          />
+
+          <p className="text-center text-sm text-gray-500">
+            {`${progress}%`} Uploading your meeting
+          </p>
+        </div>
+      ) : (
+        <>
+          <MonitorIcon className="h-10 w-10 animate-pulse" />
+
+          <h3 className="mt-2 text-sm font-semibold text-gray-900">
+            Create a new meeting
+          </h3>
+
+          <p className="mt-1 text-center text-xs text-gray-500">
+            Analyse your meeting with GithubAI
+          </p>
+
+          <div className="mt-4">
+            <Button disabled={isUploading}>
+              <UploadCloudIcon className="-ml-0.5 mr-0.5 size-4" />
+              Upload Meeting
+              <input className="hidden" {...getInputProps()} />
+            </Button>
+          </div>
+        </>
+      )}
+    </Card>
+  );
+};
+
+export default MeetingCard;
+
+{
+  /* {!isUploading && (
+        <>
+          <MonitorIcon className="h-10 w-10 animate-pulse" />
+
+          <h3 className="mt-2 text-sm font-semibold text-gray-900">
+            Create a new meeting
+          </h3>
+
+          <p className="mt-1 text-center text-xs text-gray-500">
+            Analyse your meeting with GithubAI
+          </p>
+
+          <div className="mt-4">
+            <Button disabled={isUploading}>
+              <UploadCloudIcon className="-ml-0.5 mr-0.5 size-4" />
+              Upload Meeting
+              <input className="hidden" {...getInputProps()} />
+            </Button>
+          </div>
+        </>
+      )}
+
+      {isUploading && (
+        <div className="">
+          <CircularProgressbar
+            value={progress}
+            text={`${progress}`}
+            className="size-20"
+          />
+
+          <p className="text-center text-sm text-gray-500">
+            Uploading your meeting
+          </p>
+        </div>
+      )} */
+}
